78b572a17605725491589c08d69f45686735e869
Karb0 <carbonel.mathieu@gmail.com>
2014-10-16 16:33:46 +0200

    Création d'un utilisateur et de ses tokens

--- simplonform.rb
+++ simplonform.rb
@@ -4,25 +4,33 @@ require 'mongoid'
 require 'pry'
 
 configure do
-	Mongoid.load!("./mongoid.yml")
+  Mongoid.load!("./mongoid.yml")
 end
 
+# models definition
 class User
-	include Mongoid::Document
+  include Mongoid::Document
+  before_create :generate_tokens
 
-	field :email, type: String
+  field :email, type: String
+  field :private_token, type: String
+  field :token, type: String
+
+  def generate_tokens 
+    self.private_token = SecureRandom.hex
+    self.token = SecureRandom.uuid
+  end
 end
 
+# routes & controllers
 post '/form' do
-	200
+  200
 end
 
 get "/" do
-	slim :index
+  slim :index
 end
 
 post "/" do
-	User.create(email: params[:email])
+  User.create(email: params[:email])
 end
-
-
--- test.rb
+++ test.rb
@@ -2,18 +2,14 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
 require_relative 'simplonform.rb'
- 
+
 include Rack::Test::Methods
- 
+
 def app
   Sinatra::Application
 end
 
-describe "simplonform" do 
-  it "should handle the form" do
-    post '/form', params={name: "mathieu", mail: "carbonel@gmail.com", tel: "0689347777"}
-    assert last_response.ok?
-  end
+describe "simplonform" do
 
   it "should display an email field" do 
     get '/'
@@ -21,9 +17,24 @@ describe "simplonform" do
   end
 
   it "should create a user" do
-  	users_count = User.count
-  	post "/", params={email: "yolo@yolo.com"}
-  	assert last_response.ok?
-  	assert_equal users_count + 1, User.count
+    User.delete_all
+    post "/", params = {email: "yolo@yolo.com"}
+    assert last_response.ok?
+    assert_equal 1, User.count
   end
+
+  it "should generate 2 tokens" do
+    User.delete_all
+  	post "/", params = {email: "yolo@yolo.com"}
+    assert_equal 36, User.last.token.length #uuid
+    assert_equal 32, User.last.private_token.length #hex
+  end
+
+  #it "should send an email to User.email"
+
+  it "should handle the form" do
+    post '/form', params={name: "mathieu", mail: "carbonel@gmail.com"}
+    assert last_response.ok?
+  end
+
 end
