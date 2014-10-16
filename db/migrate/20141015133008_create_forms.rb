125f6b5006643a4ba93a32af899b5a0a85eb6f84
Karb0 <carbonel.mathieu@gmail.com>
2014-10-16 10:14:42 +0200

    supr fichiers inutile

--- db/migrate/20141015133008_create_forms.rb
+++
@@ -1,9 +0,0 @@
-class CreateForms < ActiveRecord::Migration
-  def change
-  	create_table :forms do |t|
-      t.string :email
-      t.string :token
-      t.string :private_token
-    end
-	end
-end
